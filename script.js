086d40c39f118398c4f8c8153477229dcdc6278f
shamiljafarov <shamil.jafarli1@gmail.com>
2025-06-08 10:42:03 +0400

    Təsvir edən mesaj: dizayn düzəldildi, responsivlik əlavə olundu

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const text = "“Stilin burda kəsilir. Xarakterin BlackCut-da formalaşır.” – Brutal tərz, peşəkar qayçı, yalnız kişilər üçün yer.";
+// const element = document.getElementById("typing-text");
+// let index = 0;
+
+// function type() {
+//   if (index < text.length) {
+//     element.innerHTML += text.charAt(index);
+//     index++;
+//     setTimeout(type, 50);
+//   }
+// }
+
+// type();
